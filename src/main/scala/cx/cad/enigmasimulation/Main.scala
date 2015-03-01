cfb6f4d15fc68616565a9fc0f27ab302045a19a4
Colin Dean <git@cad.cx>
2015-03-01 01:57:25 -0500

    sbt 'run "welcome to the enigma"'

--- src/main/scala/cx/cad/enigmasimulation/Main.scala
+++ src/main/scala/cx/cad/enigmasimulation/Main.scala
@@ -2,6 +2,27 @@ package cx.cad.enigmasimulation
 
 object Main {
     def main(args: Array[String]) {
-      println("Hello World!!!")
+
+      if(args.length != 1){
+        println("You must supply something to encrypt.")
+        System.exit(1)
+      }
+
+      val enigma = new Enigma(Plugboard(), Reflector(), Seq(Rotor(), Rotor(), Rotor()))
+
+      val originalInput = args(0)
+      val cleanInput = originalInput.toUpperCase.replace(" ", "")
+
+      if(!cleanInput.equals(originalInput)){
+        println(
+          s"""$originalInput
+             |became
+             |$cleanInput
+           """.stripMargin)
+      }
+
+      println(enigma.crypt(cleanInput))
+
+      print(enigma.toString)
     }
 }
